a357ee8a23ce2b09b89a6e2733dc2a84de995fd7
Jviyata <viyatarutasart@gmail.com>
2026-03-03 13:15:29 -0500

    Fix base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/netflix-clone1/",
+  base: "/netxclon/",
   build: {
     outDir: "docs"
   }
